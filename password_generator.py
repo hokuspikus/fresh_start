6ffa0cb74fada3bf055ffd61f7a9f9aad5abbfff
Iwo Pikus <hokuspikus90@gmail.com>
2022-08-06 17:56:48 +0200

    modified password generator, added complexity

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,24 @@
 from random import choices
 import string
 
-characters = string.printable[:95]
+characters = string.printable[:77]
+
+COMPLEXITY = {
+    "1": characters[10:37],
+    "2": characters[:37],
+    "3": characters[:62],
+    "4": characters[:66],
+    "5": characters
+}
 
 def generate_password():
     no_of_chars = int(input("Please tell me how many characters you want in your password? "))
-    password = "".join(choices(characters, k=no_of_chars))
+    difficulty = input("How complex do you want your password? In range of 1 to 5 ")
+    if difficulty in COMPLEXITY:
+        complex_chars = COMPLEXITY[difficulty]
+    else:
+        print("Rethink your choices :)")
+    password = "".join(choices(complex_chars, k=no_of_chars))
     print(f"Your suggested password is {password}")
     return password
 
